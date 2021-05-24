dd57d76ae0d5cc73e9ad20c7e7ad014b75a0ef20
Alex Moffat <amoffat@getconvey.com>
2021-05-24 14:36:28 -0500

    Define some return types

--- python-flask/wordcount/__init__.py
+++ python-flask/wordcount/__init__.py
@@ -3,7 +3,7 @@ from .hello_world import hello_world
 from typing import Any, Mapping
 
 
-def create_app(test_config: Mapping[str, Any] = None):
+def create_app(test_config: Mapping[str, Any] = None) -> Any:
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
